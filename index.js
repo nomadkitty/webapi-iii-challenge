fa97ddd4bae9be5ced86fd6c56b9110728a0a862
Jojo Zhang <jojozhang0327@gmail.com>
2019-10-03 18:51:40 -0400

    ready to deploy

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
+require("dotenv").config();
+
 const server = require("./server");
 
-server.listen(8000, () => {
-  console.log("\n* Server Running on http://localhost:8000 *\n");
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
